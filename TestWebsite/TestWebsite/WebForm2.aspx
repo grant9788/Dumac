80a238cad5a9d0aa7d00797c116778452141bdf4
Brent Grant <31977148+grant9788@users.noreply.github.com>
2020-01-05 14:14:12 -0500

    Update

---
+++ TestWebsite/TestWebsite/WebForm2.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="TestWebsite.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+    </style>
+    <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="http://ajaxgoogleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script type="text/javascript" src="http://ajaxgoogleapis.com/ajax/libs/jquery/1.8.1/jquery-ui.min.js"></script>
+        <link href="jquery-ui.css" rel="stylesheet" type="text/css"/>
+        <script src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.8.0.js"></script>
+        <script src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.22/jquery-ui.js"></script>
+
+        <script src="~/Scripts/jquery-1.10.2.min.js"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
+        <script src="https://codejquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+
+        <script type="text/javascript">
+            $(function () {
+                $("#txtDescription").autocomplete({
+                    source: function (request, response) {
+                        var param = { Description: $('#txtDescription').val() };
+                        $.ajax({
+                            url: "WebForm2.aspx/GetDescription",
+                            data: JSON.stringify(param),
+                            dataType: "json",
+                            type: "POST",
+                            contentType: "application/json; charset=utf-8",
+                            dataFilter: function (data) { return data; },
+                            success: function (data) {
+                                response($.map(data.d, function (item) {
+                                    return {
+                                        value: item
+                                    }
+                                }))
+                            },
+                            error: function (XMLHttpRequest, textstatus, errorThrown) {
+                                alert(errorThrown);
+                            }
+
+                        });
+                    },
+                    minLength: 2
+                });
+            });
+        </script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+    <div class="auto-style1">
+    
+        <asp:Label ID="Label1" runat="server" Text="Expenses"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Date"></asp:Label>
+        <asp:TextBox ID="txtDate" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Description"></asp:Label>
+        <asp:TextBox ID="txtDescription" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Catgory"></asp:Label>
+        <asp:TextBox ID="txtCatgory" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Amount"></asp:Label>
+        <asp:TextBox ID="txtAmount" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click" />
+        <br />
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="Submited"></asp:Label>
+    
+        <br />
+        <asp:Label ID="Label7" runat="server" style="text-align: left" Text="Label"></asp:Label>
+    
+    </div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField DataField="Date" HeaderText="Date" SortExpression="Date" />
+                <asp:BoundField DataField="Description" HeaderText="Description" SortExpression="Description" />
+                <asp:BoundField DataField="Category" HeaderText="Category" SortExpression="Category" />
+                <asp:BoundField DataField="Amount" HeaderText="Amount" SortExpression="Amount" />
+            </Columns>
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:CamudConnectionString %>" SelectCommand="SELECT * FROM [CamudProject]" OnSelecting="SqlDataSource1_Selecting"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ TestWebsite/TestWebsite/WebForm2.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Services;
+using System.Configuration;
+using System.Data.OleDb;
+
+namespace TestWebsite
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CamudConnectionString"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into CamudProject (Date,Description,Category,Amount) values('"+txtDate.Text+"','"+txtDescription.Text+"','"+txtCatgory.Text+"','"+txtAmount.Text+"')",con); // Error happen here User Input is More then what is in Database set 
+           // SqlCommand cmd = new SqlCommand("insert into CamudProject values('" + txtDate.Text + "','" + txtDescription.Text + "','" + txtCatgory + "','" + txtAmount + "') ", con);
+           
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Label7.Visible = true;
+            Label7.Text = "Success";
+            gridview_data();
+            //REFORMAT IT TO A BLANK 
+            txtDate.Text = "";
+            txtDescription.Text = "";
+            txtCatgory.Text = "";
+            txtAmount.Text = "";
+
+            
+        }
+
+        public void gridview_data() 
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CamudConnectionString"].ConnectionString);
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("Select * from CamudProject",con);
+            //SqlCommand refresh = new SqlCommand("Select * from CamudProject", con);
+            //refresh.ExecuteNonQuery();
+
+
+            DataSet1 ds = new DataSet1();
+            da.Fill(ds, "insert_data");
+            GridView1.DataSource = ds;
+            GridView1.DataMember = "insert_data";
+            GridView1.DataSourceID = null;
+            //grid.DataSourceID = null;
+            //grid.DataSource = dataTable;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+
+
+        [WebMethod]
+        public static List<string> GetDescription(string Description)
+        {
+            List<string> D1escription = new List<string>();
+            OleDbConnection con2 = new OleDbConnection(@"Provider=SQLOLEDB; Data Source=DESKTOP-O9IMT6I;Initial Catalog=Camud;Integrated Security=SSPI");
+            
+            string query = string.Format("Select * From CamudProject where Description like '%{0}%'", Description);
+            OleDbCommand cmdE = new OleDbCommand(query, con2);
+            con2.Open();
+            OleDbDataReader reader = cmdE.ExecuteReader();
+            while(reader.Read())
+            {
+                D1escription.Add(string.Format("{0}", reader["Description"]));
+                //D1escription.Add(reader.GetString(0));
+                //Description.Add(string.Format("{0}", sdr["Description"]));
+            }
+            return D1escription;
+
+        }
+
+
+        //[WebMethod]
+        //public static string[] GetDescription(string prefix)
+        //{
+        //    List<string> Description = new List<string>();
+        //    using (SqlConnection conn = new SqlConnection())
+        //    {
+        //        conn.ConnectionString = ConfigurationManager.ConnectionStrings["CamudConnectionString"].ConnectionString;
+        //        using (SqlCommand cmd = new SqlCommand())
+        //        {
+        //            cmd.CommandText = "select * from CamudProject where Description";
+        //            cmd.Parameters.AddWithValue("@Description", prefix);
+        //            cmd.Connection = conn;
+        //            conn.Open();
+        //            using (SqlDataReader sdr = cmd.ExecuteReader())
+        //            {
+        //                while (sdr.Read())
+        //                {
+        //                    Description.Add(string.Format("{0}", sdr["Description"]));
+        //                }
+        //            }
+        //            conn.Close();
+        //        }
+        //    }
+        //    return Description.ToArray();
+        //}
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //SqlCommand refresh = new SqlCommand("Select * from CamudProject",con);
+            //refresh.ExecuteNonQuery();
+            //con.Close();
+
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+    }
+}
